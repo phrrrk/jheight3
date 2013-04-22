0ba189a6f7d1f021034ed52cb69534b074def498
phrrrk <phrk.inc@gmail.com>
2013-04-22 09:02:49 +0200

    Commit before major changes

--- jheight3tester.m
+++ jheight3tester.m
@@ -5,7 +5,7 @@ for i = 16:16
     trialName = ['Rohdaten/sub_no_', num2str(i), '_trial_', num2str(k), '.csv'];
     dataList{i-15,1} = trialName;
     try
-      jumpHeight = jheight3(trialName);
+      jumpHeight = jheight3(trialName, [50 500], 0, 0, 0, 0, 0, 1);
       dataList{i-15,k+1} = jumpHeight;
     end
   end
